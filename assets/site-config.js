299d6c03222416af548ebf4489233b131f758339
3btbaylocks <btbaylock@gmail.com>
2026-08-07 16:19:35 -0400

    Add 15-minute Calendly listening session

--- assets/site-config.js
+++ assets/site-config.js
@@ -3,6 +3,7 @@ window.BB_CONFIG={
   "ownerIntroUrl": "https://docs.google.com/forms/d/e/1FAIpQLSeaavKEEIKA99JVQexKvtBYWt7dooTHFELQ477gIsXbqjROxQ/viewform?usp=pp_url&entry.1926571282=Owner+Advisory",
   "communityIntroUrl": "https://docs.google.com/forms/d/e/1FAIpQLSeaavKEEIKA99JVQexKvtBYWt7dooTHFELQ477gIsXbqjROxQ/viewform?usp=pp_url&entry.1926571282=Receiving+Community",
   "dischargeIntroUrl": "https://docs.google.com/forms/d/e/1FAIpQLSeaavKEEIKA99JVQexKvtBYWt7dooTHFELQ477gIsXbqjROxQ/viewform?usp=pp_url&entry.1926571282=Discharge+Organization",
+  "calendlyUrl": "https://calendly.com/brantbaylock/new-meeting",
   "linkedinUrl": "https://www.linkedin.com/in/brantbaylock/",
   "contentApiUrl": "",
   "gaMeasurementId": "G-DLS63RH84T",
